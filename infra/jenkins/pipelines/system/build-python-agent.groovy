9f1a8a749d578c52078c53066b5fe3af4cf6437d
Lukasz Brzezinski <lukaszz.brzezinski@gmail.com>
2025-10-13 16:39:22 +0200

    change agent pipeline definition

--- infra/jenkins/pipelines/system/build-python-agent.groovy
+++ infra/jenkins/pipelines/system/build-python-agent.groovy
@@ -3,9 +3,20 @@ def registry = 'localhost:5000'
 def imageName = 'jenkins/inbound-agent'
 def imageTag = 'jdk17-helm-make'
 
-load 'infra/jenkins/pipelines/templates/build-agent-template.groovy' with parameters: [
-    DOCKERFILE_NAME: dockerfile,
-    IMAGE_NAME: imageName,
-    IMAGE_TAG: imageTag,
-    REGISTRY: registry
-]
+load 'infra/jenkins/pipelines/templates/build-agent-template.groovy'
+
+pipeline {
+    agent any
+    stages {
+        stage('Build and Push Python Agent') {
+            steps {
+                build job: 'pipelines/templates/build-agent-template', parameters: [
+                    string(name: 'DOCKERFILE', value: dockerfile),
+                    string(name: 'IMAGE_NAME', value: imageName),
+                    string(name: 'IMAGE_TAG', value: imageTag),
+                    string(name: 'REGISTRY', value: registry)
+                ]
+            }
+        }
+    }
+}
